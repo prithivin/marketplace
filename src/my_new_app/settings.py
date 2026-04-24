06989f450759f594e4326b7dc0940de98b96a56d
PRITHIVIN LAKSHMINARAYANAN <prithivin.trustgrid@outlook.com>
2026-04-24 21:23:31 +0530

    Updated database in settings

--- src/my_new_app/settings.py
+++ src/my_new_app/settings.py
@@ -91,7 +91,7 @@ WSGI_APPLICATION = 'my_new_app.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': '/home/site/wwwroot/db.sqlite3',
     }
 }
 
